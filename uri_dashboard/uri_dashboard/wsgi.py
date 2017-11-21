849c51deb587bb8a9081131a2ea5466d13e9c5f0
lukasavicus <lukasavicus@gmail.com>
2017-11-21 00:04:48 -0200

    Added a Procfile.

--- uri_dashboard/uri_dashboard/wsgi.py
+++ uri_dashboard/uri_dashboard/wsgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uri_dashboard.settings")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uri_dashboard.settings")
 
 application = get_wsgi_application()
